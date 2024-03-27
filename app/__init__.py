fd608440d30fa7c9b4947ec32227d01777ddf273
Adrian Ramos <adrianramosrp@gmail.com>
2024-03-27 16:44:45 +0100

    Add coverage gh action

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-import sys
-from pathlib import Path
-
-# Add lib and app folders to path
-LIBS_DIR = (Path(__file__).resolve()
-            .parent  # ./app
-            .parent  # ./{root}
-            )
-sys.path.append(str(LIBS_DIR))
